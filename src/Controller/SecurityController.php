e5e7ba4eebf5c04cdbc398096a0ef571992074b7
guillaumesoullard <guillaume.soullard@gmail.com>
2020-01-02 21:49:39 +0100

    heroku prod testing

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -3,21 +3,33 @@
 namespace App\Controller;
 
 use ApiPlatform\Core\Api\IriConverterInterface;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SecurityController extends AbstractController
 {
+    private $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
 
     /**
      * @Route("/", name="home")
      */
-    public function index()
+    public function index(LoggerInterface $logger)
     {
+        $this->logger->info('logging from home');
+        $user = $this->getUser();
         $za = 'dsq';
         $za = 'dsq';
-        return new Response(var_dump(phpinfo()));
+        //return new Response(var_dump(phpinfo()));
+        return $this->render('security/index.html.twig', [
+            'controller_name' => 'SecurityController',
+        ]);
     }
 
     /**
@@ -33,8 +45,9 @@ class SecurityController extends AbstractController
     /**
      * @Route("/login", name="app_login", methods={"POST", "GET"})
      */
-    public function login(IriConverterInterface $iriConverter)
+    public function login(IriConverterInterface $iriConverter, LoggerInterface $logger)
     {
+        $logger->info('I just got the logger');
         $za = $this->getUser();
         $a = 'dsq';
         return new Response(null, 204, [
